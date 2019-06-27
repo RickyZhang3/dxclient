e1a9cd9ae0d1990ad6c461c8a465e5c6fa7b780a
zhangxinghua9999@163.com <zhangxinghua9999@163.com>
2019-06-28 01:31:10 +0800

    jdbc init

--- com-dx-web/src/main/java/org/com/dx/Application.java
+++ com-dx-web/src/main/java/org/com/dx/Application.java
@@ -1,14 +1,32 @@
 package org.com.dx;
 
+import javax.sql.DataSource;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+	
+	@Autowired
+    private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+    public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+	    dataSource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
+	    dataSource.setUrl(env.getProperty("spring.datasource.url"));
+	    dataSource.setUsername(env.getProperty("spring.datasource.username"));
+	    dataSource.setPassword(env.getProperty("spring.datasource.password"));
+	    return dataSource;
+    }
 
 }
--- com-dx-web/src/main/java/org/com/dx/web/HelloController.java
+++ com-dx-web/src/main/java/org/com/dx/web/HelloController.java
@@ -2,6 +2,8 @@ package org.com.dx.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,14 @@ public class HelloController {
 	
 	private static final Logger log = LoggerFactory.getLogger(HelloController.class);
 	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@RequestMapping("/hello")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public Integer index() {
+		
+		int count = jdbcTemplate.queryForObject("select count(*) from DMP_EMPLOYEE_SOURCE", Integer.class);
+		log.info("测试数据库查询语句:{}",count);
+        return count;
     }
 }
